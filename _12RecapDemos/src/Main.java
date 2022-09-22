046db385256a346eb150b5261ec44c805749d347
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2022-09-22 18:10:30 +0300

    Java
    Java

---
+++ _12RecapDemos/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.Topla(3,4);
+        int sonuc2 = dortIslem.Cikar(15,7);
+        int sonuc3 = dortIslem.Carp(100,50);
+
+
+        System.out.println(sonuc);
+
+
+    }
+}
